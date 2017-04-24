87949af674967b232dde4b2a8efda73412e4bc3c
Ricardo Mayer <ricardomayerb@gmail.com>
2017-04-23 23:59:36 -0300

    read at last ifs

--- scripts/read_ifs_data.R
+++ scripts/read_ifs_data.R
@@ -1,13 +1,57 @@
 library(readr)
+library(dplyr)
 
 char_pos = rep("c",5)
-num_pos = rep("d", 1991)
+num_pos = rep("d", 1191)
+allpos = c(char_pos, num_pos)
 
-ifs_coltypes = c(char_pos, num_pos)
 
-IFS_yr_qr_m <- read_csv("V:/USR/RMAYER/cw/IFS/IFS_03-27-2017 20-11-19-75_timeSeries.csv",
-col_types = ifs_coltypes)
+ifs_coltypes = cols("Country Name" =  "c", "Country Code" =  "c",
+                    "Indicator Name" =  "c", "Indicator Code" =  "c",
+                    "Attribute" = "c", .default = col_double())
 
+
+# ifs_coltypes = cols(c(char_pos, num_pos)[])
+
+
+IFS_yr_qr_m_from_1980 <- read_csv("../IFS/IFS_03-27-2017_timeSeries_from_1980.csv",
+                        col_names = TRUE,
+                        ifs_coltypes)
+
+IFS_yr_qr_m_from_1980 <- IFS_yr_qr_m_from_1980 %>% 
+  select(-c(`2017M3`:`2017M12`))
+
+IFS_q_from_1980 <- IFS_yr_qr_m_from_1980 %>% 
+  select(contains("Q"))
+
+IFS_m_from_1980 <- IFS_yr_qr_m_from_1980 %>% 
+  select(contains("M"))
+
+IFS_y_from_1980 <- IFS_yr_qr_m_from_1980 %>% 
+  select(one_of(as.character(1980:2016)) )
+
+IFS_yr_qr_m_from_1948 <- read_csv("../IFS/IFS_03-27-2017_timeSeries.csv",
+                                  col_names = TRUE,
+                                  ifs_coltypes)
+
+IFS_yr_qr_m_from_1948 <- IFS_yr_qr_m_from_1948 %>% 
+  select(-c(`2017M3`:`X1196`))
+
+IFS_q_from_1948 <- IFS_yr_qr_m_from_1948 %>% 
+  select(contains("Q"))
+
+IFS_m_from_1948 <- IFS_yr_qr_m_from_1948 %>% 
+  select(contains("M"))
+
+IFS_y_from_1948 <- IFS_yr_qr_m_from_1948 %>% 
+  select(one_of(as.character(1948:2016)) )
+
+
+# IFS_yr_qr_m[[ , 6:1196]] <- as.numeric(IFS_yr_qr_m[[ , 6:1196]]) 
 # View(IFS_yr_qr_m)
 
-save(IFS_03_27_2017_20_11_19_75_timeSeries, file = "V:/USR/RMAYER/cw/IFS/IFS_yr_qr_m.rda")
+save(IFS_y_from_1980, IFS_q_from_1980, IFS_m_from_1980, 
+     file = "../IFS/IFS_yr_qr_m_from_1980.rda")
+
+save(IFS_y_from_1948, IFS_q_from_1948, IFS_m_from_1948, 
+     file = "../IFS/IFS_yr_qr_m_from_1948.rda")
