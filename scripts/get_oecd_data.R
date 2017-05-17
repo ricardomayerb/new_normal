ae034031711b0397a4d68f04f0bb19169593d010
ricardomayerb <ricardomayerb@gmail.com>
2017-05-17 18:30:14 -0300

    comienzo household debt
    edicion general

--- scripts/get_oecd_data.R
+++ scripts/get_oecd_data.R
@@ -3,9 +3,14 @@ library(dplyr)
 
 data_sets <- get_datasets()
 with_GDP <- search_dataset("GDP", data = data_sets)
+with_Glance <- search_dataset("Glance", data = data_sets)
 dset <- "EO"
-dstruct <- get_data_structure(dset)
+dstruct <- get_data_structure("NAAG")
 str(dstruct, max.level = 1)
+
+# National Accounts at a Glance
+nacc_at_glance <- get_dataset("NAAG") 
+
 
 eo_nov_2016 <- get_dataset("EO")
 
@@ -13,6 +18,7 @@ productivity_gr_dataset <- get_dataset("PDB_GR")
 
 productivity_lvl_dataset <- get_dataset("PDB_LV")
 
+save(nacc_at_glance, file = "./produced_data/oecd_naccag")
 
 
 save(eo_nov_2016, productivity_gr_dataset, productivity_lvl_dataset ,
