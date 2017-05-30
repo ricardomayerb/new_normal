672673e4f79366fcc0eb9cfe2748c981215e0188
Ricardo Mayer <ricardomayerb@gmail.com>
2017-05-30 04:37:33 -0400

    plot nrf vs struc

--- scripts/wrangle_bop_dde.R
+++ scripts/wrangle_bop_dde.R
@@ -118,7 +118,8 @@ save(fin_flows_18_xts, fin_flows_long_lac_18_yearly, fin_flows_long_lac_18,
 
 #---- 
 # by countries
-new_dates <- as.yearqtr(2000 + seq(0,67/4, by = 1/4))
+new_dates_2000 <- as.yearqtr(2000 + seq(0,67/4, by = 1/4))
+new_dates_1993 <- as.yearqtr(1993 + seq(0,95/4, by = 1/4))
 wbpath  = "./raw_data/Bal Pag_TRIMESTRAL_CECILIA_rm.xlsx"
 rangedata <- "BI8:EZ62"
 
@@ -134,6 +135,10 @@ cstart <-  1
 cend <- 156
 
 dfs_bop <- list_along(iso_names)
+dfs_bop_long <- list_along(iso_names)
+dfs_bop_tidy <- list_along(iso_names)
+
+all_countries_tidy <- data_frame()
 
 for (i in seq_along(dfs_bop) ) {
   
@@ -150,12 +155,61 @@ for (i in seq_along(dfs_bop) ) {
                            col_names = FALSE) 
   
   names_catdata <- c("rubroold", "grupo", "rubro", "manual", "pais")
-  names_portiondata <- new_dates
-  
-  # names(portiondata) <- names_portiondata
-  # names(catdata) <- names_catdata
+  names_portiondata <- new_dates_1993
   
   countrytable <- bind_cols(catdata , portiondata)
+  names(countrytable) <- c(c(names_catdata), c(names_portiondata))
+  
+  countrytable_long <- countrytable %>% 
+    gather(key = yearq, value = value,
+           -c(rubroold, grupo, rubro, manual, pais))
+  
+  countrytable_tidy <- countrytable_long %>% 
+    select(-c(rubroold, manual, grupo)) %>% 
+    spread(key = rubro, value = value)
+ 
   dfs_bop[[i]] <- countrytable 
+  dfs_bop_long[[i]] <- countrytable_long 
+  dfs_bop_tidy[[i]] <- countrytable_tidy
 }
+names(dfs_bop) <- iso_names
+names(dfs_bop_long) <- iso_names
+names(dfs_bop_tidy) <- iso_names
 
+bop_lac_all_countries_tidy <- do.call("rbind", dfs_bop_tidy) %>% 
+  mutate(year = floor(as.numeric(yearq)))
+
+non_residents_flows_cols <- c(1,2,23,26,27, 58)
+residents_flows_cols <- c(1,2 , 22,3,4, 58)
+
+nrf_lac_all_countries <- bop_lac_all_countries_tidy[,non_residents_flows_cols]
+rf_lac_all_countries <- bop_lac_all_countries_tidy[,non_residents_flows_cols]
+
+nrf_all <- bop_lac_all_countries_tidy %>% 
+  select(non_residents_flows_cols) 
+
+rf_all <- bop_lac_all_countries_tidy %>% 
+  select(residents_flows_cols) 
+
+names(nrf_all)[c(3,4,5)] <- c("ied_eed", "p_cartera", "p_otra")
+
+names(rf_all)[c(3,4,5)] <- c("ied_eee", "a_cartera", "a_otra")
+
+nrf_by_country_year <- nrf_all %>% 
+  group_by(pais, year) %>% 
+  summarise(ied_eed_y = sum(as.numeric(ied_eed), na.rm = TRUE),
+            p_cartera_y = sum(as.numeric(p_cartera), na.rm = TRUE),
+            p_otra_y = sum(as.numeric(p_otra), na.rm = TRUE),
+            total_nrf = ied_eed_y + p_cartera_y + p_otra_y)
+
+rf_by_country_year <- rf_all %>% 
+  group_by(pais, year) %>% 
+  summarise(ied_eee_y = sum(as.numeric(ied_eee), na.rm = TRUE),
+            a_cartera_y = sum(as.numeric(a_cartera), na.rm = TRUE),
+            a_otra_y = sum(as.numeric(a_otra), na.rm = TRUE),
+            total_rf = ied_eee_y + a_cartera_y + a_otra_y)
+
+
+
+save(bop_lac_all_countries_tidy, nrf_by_country_year,
+     rf_by_country_year, file = "./produced_data/all_bops_lac")
